497c63166478f192ce7e7e704b74e3dac9abf287
VARSHA-KUSHWAHA <139112844+VARSHA-KUSHWAHA@users.noreply.github.com>
2024-07-17 23:19:42 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const hbs = require("hbs");
+const path = require("path");
 require("dotenv").config();
 const app = express();
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.json());
 console.log("process.env.PORT",process.env.PORT);
 const userRouter=require("./routes/user.route");
